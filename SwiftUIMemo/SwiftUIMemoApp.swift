67ed181203c2671d7a18ecfe3d64ad58f51c17ff
kmsdoit <klmmms882912@naver.com>
2023-02-24 18:41:59 +0900

    Initial Commit

---
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIMemoApp.swift
+//  SwiftUIMemo
+//
+//  Created by 김명수 on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
